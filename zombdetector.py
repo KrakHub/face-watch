69a0fd999aad0851cf6daf85591907d1d457dcd9
Kraken <superajc@outlook.com>
2022-02-25 08:31:51 -0500

    Alexs Contribution

--- zombdetector.py
+++ zombdetector.py
@@ -1,30 +1,33 @@
 import face_recognition
+import os
 
-zomblib_joeBiden = face_recognition.load_image_file("Programming\Projects\Zombies Among Us\zombie-library\Joe Biden.jpg")
-zomblib_donaldTrump = face_recognition.load_image_file("Programming\Projects\Zombies Among Us\zombie-library\Donald Trump.jpg")
-zomblib_kimUn = face_recognition.load_image_file("Programming\Projects\Zombies Among Us\zombie-library\Kim Jong Un.jpg")
-zomblib_vladimirPutin = face_recognition.load_image_file("Programming\Projects\Zombies Among Us\zombie-library\Vladimir Putin.jpg")
+from sqlalchemy import table
+
+path = "Programming\Projects\Zombies Among Us\zombie-library"
+AllPics = os.listdir(path)
+
+Faces = []
+known_faces = []
+
+print("LOADING IMAGES")
+
+for v in AllPics:
+    imageFile = face_recognition.load_image_file("Programming\Projects\Zombies Among Us\zombie-library\\" + v)
+    Encoding = face_recognition.face_encodings(imageFile)[0]
+    known_faces.append(Encoding)
+    Faces.append(v.rsplit('.', 1)[0])
+
+print("IMAGES HAVE LOADED")
 
 zomblib_detect = face_recognition.load_image_file("Programming\Projects\Zombies Among Us\detect\detect.jpg")
+zombencode_detect = face_recognition.face_encodings(zomblib_detect)[0]
 
-try:
-    zombencode_joeBiden = face_recognition.face_encodings(zomblib_joeBiden)[0]
-    zombencode_donaldTrump = face_recognition.face_encodings(zomblib_donaldTrump)[0]
-    zombencode_kimUn = face_recognition.face_encodings(zomblib_kimUn)[0]
-    zombencode_vladimirPutin = face_recognition.face_encodings(zomblib_vladimirPutin)[0]
-
-    zombencode_detect = face_recognition.face_encodings(zomblib_detect)[0]
-except IndexError:
-    print('Error')
-    quit()
-
-known_faces = [
-    zombencode_donaldTrump,
-    zombencode_joeBiden,
-    zombencode_kimUn,
-    zombencode_vladimirPutin
-]
+print("GETTING RESULTS")
 
 results = face_recognition.compare_faces(known_faces, zombencode_detect)
+index = 0
 
-print(results[0])
+for result in results:
+    if result == True:
+        print(Faces[index] + " was included in this picture!")
+    index += 1
