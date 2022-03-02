373582bb8b0e5a084dbe43637ac3c6f8beb7d895
Kraken <superajc@outlook.com>
2022-03-02 08:32:01 -0500

    Imported webcam code, will edit later

--- zombdetector.py
+++ zombdetector.py
@@ -1,13 +1,19 @@
 import face_recognition
+import cv2
 import os
+import numpy as np
 
 from sqlalchemy import table
+
+video_capture = cv2.VideoCapture(0)
 
 path = "Programming\Projects\Zombies Among Us\zombie-library"
 AllPics = os.listdir(path)
 
 Faces = []
 known_faces = []
+face_locations = []
+process_currentframe = True
 
 print("LOADING IMAGES")
 
@@ -19,15 +25,66 @@ for v in AllPics:
 
 print("IMAGES HAVE LOADED")
 
-zomblib_detect = face_recognition.load_image_file("Programming\Projects\Zombies Among Us\detect\detect.jpg")
-zombencode_detect = face_recognition.face_encodings(zomblib_detect)[0]
+while True:
+    ret, frame = video_capture.read()
 
-print("GETTING RESULTS")
+    small_frame = cv2.resize(frame, (0,0), fx=0.25, fy=0.25)
 
-results = face_recognition.compare_faces(known_faces, zombencode_detect)
-index = 0
+    rgb_small_frame = small_frame[:, :, ::-1]
 
-for result in results:
-    if result == True:
-        print(Faces[index] + " was included in this picture!")
-    index += 1
+    if process_currentframe:
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        for face_encoding in face_encodings:
+            match = face_recognition.compare_faces(known_faces, face_encodings)
+            name = "Unknown"
+
+            face_distances = face_recognition.face_distance(known_faces, face_encodings)
+            best_match_index = np.argmin(face_distances)
+            if match[best_match_index]:
+                name = Faces[best_match_index]
+                print(name)
+
+            Faces.append(name)
+
+        process_currentframe = not process_currentframe
+
+        for (top, right, bottom, left), name in zip(face_locations, Faces):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    # Display the resulting image
+    cv2.imshow('Video', frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
+
+# zomblib_detect = face_recognition.load_image_file("Programming\Projects\Zombies Among Us\detect\detect.jpg")
+# zombencode_detect = face_recognition.face_encodings(zomblib_detect)[0]
+
+# print("GETTING RESULTS")
+
+# results = face_recognition.compare_faces(known_faces, zombencode_detect)
+# index = 0
+
+# for result in results:
+#     if result == True:
+#         print(Faces[index] + " was included in this picture!")
+#     index += 1
