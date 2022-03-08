15017e2180de6e94ffcd84a165dc6c614b7b91d9
Kraken <superajc@outlook.com>
2022-03-08 11:44:54 -0500

    Added some comments on zombdetector.py

--- zombdetector.py
+++ zombdetector.py
@@ -8,20 +8,20 @@ from sqlalchemy import table
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate("Programming\Projects\Zombies Among Us\Key.json")
+cred = credentials.Certificate("Key.json")
 firebaseadmin = firebase_admin.initialize_app(cred, {'databaseURL': 'https://faces-c07d3-default-rtdb.firebaseio.com'})
 
 from firebase_admin import db
 
 video_capture = cv2.VideoCapture(0)
 
-path = "Programming\Projects\Zombies Among Us\zombie-library"
-AllPics = os.listdir(path)
+#path = "Programming\Projects\Zombies Among Us\zombie-library"
+#AllPics = os.listdir(path)
 
-Faces = []
+Faces = [] #A constant, list of all of the face names
 known_faces = []
 face_locations = []
-face_names = []
+face_names = [] #
 process_currentframe = True
 
 print("LOADING IMAGES")
@@ -38,25 +38,26 @@ for name in data:
     known_faces.append(np.array(encoding))
     Faces.append(name)
 
-#for v in AllPics:
-    #imageFile = face_recognition.load_image_file("Programming\Projects\Zombies Among Us\zombie-library\\" + v)
-    #Encoding = face_recognition.face_encodings(imageFile)[0]
-    #known_faces.append(Encoding)
-    # send data to firebase
-    #ref = db.reference("/" + v.rsplit('.', 1)[0])
-    #print(Encoding)
-    #ref.set({"Encoding": Encoding.tolist()})
-    #Faces.append(v.rsplit('.', 1)[0])
+#!for v in AllPics:
+    #!imageFile = face_recognition.load_image_file("Programming\Projects\Zombies Among Us\zombie-library\\" + v)
+    #!Encoding = face_recognition.face_encodings(imageFile)[0]
+    #!known_faces.append(Encoding)
+    #! send data to firebase
+    #!ref = db.reference("/" + v.rsplit('.', 1)[0])
+    #!print(Encoding)
+    #!ref.set({"Encoding": Encoding.tolist()})
+    #!Faces.append(v.rsplit('.', 1)[0])
 
 print("IMAGES HAVE LOADED")
 
 while True:
+    #Reads the current video capture, and sets it to RGB format
     ret, frame = video_capture.read()
 
     rgb_small_frame = frame[:, :, ::-1] 
 
-    if process_currentframe:
-        face_locations = face_recognition.face_locations(rgb_small_frame)
+    if process_currentframe: #The codes only run if the frame is set to be read on
+        face_locations = face_recognition.face_locations(rgb_small_frame) 
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
         face_names=[]
@@ -69,7 +70,7 @@ while True:
             if match[best_match_index]:
                 name = Faces[best_match_index]
 
-            face_names.append(name)
+            face_names.append(name) #face_names will be the names of the faces currently detected
 
     process_currentframe = not process_currentframe
 
@@ -81,7 +82,7 @@ while True:
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-    cv2.imshow('Video', frame)
+    cv2.imshow('Video', frame) #Displays the video
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
@@ -90,15 +91,15 @@ while True:
 video_capture.release()
 cv2.destroyAllWindows()
 
-# zomblib_detect = face_recognition.load_image_file("Programming\Projects\Zombies Among Us\detect\detect.jpg")
-# zombencode_detect = face_recognition.face_encodings(zomblib_detect)[0]
+#! zomblib_detect = face_recognition.load_image_file("Programming\Projects\Zombies Among Us\detect\detect.jpg")
+#! zombencode_detect = face_recognition.face_encodings(zomblib_detect)[0]
 
-# print("GETTING RESULTS")
+#! print("GETTING RESULTS")
 
-# results = face_recognition.compare_faces(known_faces, zombencode_detect)
-# index = 0
+#! results = face_recognition.compare_faces(known_faces, zombencode_detect)
+#! index = 0
 
-# for result in results:
-#     if result == True:
-#         print(Faces[index] + " was included in this picture!")
-#     index += 1
+#! for result in results:
+#!     if result == True:
+#!         print(Faces[index] + " was included in this picture!")
+#!     index += 1
