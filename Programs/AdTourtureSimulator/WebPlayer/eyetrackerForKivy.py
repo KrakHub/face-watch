1fef3a1fb75f7a2083392bf19ee6e66a3599a150
Kraken <superajc@outlook.com>
2022-04-01 00:13:52 -0400

    Eye tracker runs from the browser now, only issue is getting it to constantly scan

--- Programs/AdTourtureSimulator/WebPlayer/eyetrackerForKivy.py
+++ Programs/AdTourtureSimulator/WebPlayer/eyetrackerForKivy.py
@@ -1,47 +1,42 @@
 from gaze_tracking import GazeTracking
 import cv2
-import firebase_admin
-from firebase_admin import credentials
-from sqlalchemy import false
+# import firebase_admin
+# from firebase_admin import credentials
 
-cred = credentials.Certificate("Key.json")
-firebaseadmin = firebase_admin.initialize_app(cred, {'databaseURL': 'https://faces-c07d3-default-rtdb.firebaseio.com'})
+# cred = credentials.Certificate("Key.json")
+# firebaseadmin = firebase_admin.initialize_app(cred, {'databaseURL': 'https://faces-c07d3-default-rtdb.firebaseio.com'})
 
-from firebase_admin import db
+# from firebase_admin import db
 
-gaze = GazeTracking()
-video_capture = cv2.VideoCapture(0)
+def runEyetracker():
+    return 'Hello World!'
 
-process_currentframe = 0
 
-opened = False
 
-while True:
-    ret, frame = video_capture.read()
 
-    if process_currentframe >= 1: 
-        process_currentframe = 0
 
-        gaze.refresh(frame)
-        frame = gaze.annotated_frame()
+def runEyetrackerForReal():
+    process_currentframe = 0
+    gaze = GazeTracking()
+    video_capture = cv2.VideoCapture(0)
+    while True:
+        ret, frame = video_capture.read()
 
-        text = ""
-        if gaze.is_right():
-            text = "Looking right"
-        elif gaze.is_left():
-            text = "Looking left"
-        elif gaze.is_center():
-            text = "Looking center"
-        elif gaze.is_blinking():
-            text = "Blinking"
-        print(text)
+        if process_currentframe >= 1: 
+            process_currentframe = 0
 
-    process_currentframe += 1
+            gaze.refresh(frame)
+            frame = gaze.annotated_frame()
 
-    cv2.imshow('Video', frame) #Displays the video
+            text = ""
+            if gaze.is_right():
+                text = "Looking right"
+            elif gaze.is_left():
+                text = "Looking left"
+            elif gaze.is_center():
+                text = "Looking center"
+            elif gaze.is_blinking():
+                text = "Blinking"
+            return text
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-video_capture.release()
-cv2.destroyAllWindows()
+        process_currentframe += 20
--- Programs/AdTourtureSimulator/WebPlayer/main.py
+++ Programs/AdTourtureSimulator/WebPlayer/main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
+from eyetrackerForKivy import *
 app = Flask(__name__)
 
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate("Key.json")
+cred = credentials.Certificate("Programs/AdTourtureSimulator/WebPlayer/key2.json")
 firebaseadmin = firebase_admin.initialize_app(cred, {'databaseURL': 'https://accounts-883b3-default-rtdb.firebaseio.com'})
 
 from firebase_admin import db
@@ -17,12 +18,12 @@ def home():
     page = "home"
     return render_template('home.html')
 
-@app.route('/login', methods=["POST", "GET"])
+@app.route('/login', methods=["GET", "POST"])
 def login():
     global page
-
+    print('Entered Login')
     if request.method == "POST" and page == "login":
-        user = request.form["nm"]
+        user = request.form["name"]
         Pass = request.form["pass"]
         
         ref = db.reference("/")
@@ -40,6 +41,7 @@ def login():
                 break
 
         if valid:
+            print('VALID!')
             return redirect(url_for("user", usr=user))
         else:
             return redirect(url_for("login", usr=user))
@@ -47,7 +49,7 @@ def login():
         page = "login"
         return render_template("login.html")
 
-@app.route('/signup', methods=["POST", "GET"])
+@app.route('/signup', methods=["GET", "POST"])
 def signup():
     global page
     print(page)
@@ -67,8 +69,7 @@ def signup():
 @app.route('/<usr>')
 def user(usr):
     global page
-    #page = "user"
-    return render_template("user.html", user=usr, videos={"SpongeBob", "Squarepants"})
+    return render_template("user.html", user=usr, videos={"SpongeBob", "Squarepants"}, text=runEyetrackerForReal())
 
 
 if __name__ == '__main__':
